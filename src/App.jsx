7045bd7b28662b019711794557b293563e927b84
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2024-08-19 22:41:38 +0530

    Add mirage server

--- src/App.jsx
+++ src/App.jsx
@@ -12,20 +12,22 @@ import Income from "./pages/Host/Income"
 import HostVans from "./pages/Host/HostVans"
 import Reviews from "./pages/Host/Reviews"
 
+import "../server"
+
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainLayout />}>
+          <Route index element={<Home />} />
           <Route path="host" element={<HostLayout />}>
             <Route index element={<Dashboard />} />
             <Route path="income" element={<Income />} />
             <Route path="vans" element={<HostVans />} />
             <Route path="reviews" element={<Reviews />} />
           </Route>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/vans" element={<Vans />} />
+          <Route path="about" element={<About />} />
+          <Route path="vans" element={<Vans />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/pages/Host/HostVans.jsx
+++ src/pages/Host/HostVans.jsx
@@ -1,3 +1,11 @@
+import React from "react"
+
 export default function HostVans() {
+  React.useEffect(() => {
+    fetch("/api/vans")
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+  })
+
   return <h1>Host Vans Page</h1>
 }
