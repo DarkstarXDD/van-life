08d993192d97eb69c55d5c5de1438acd5da1c5ae
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2024-08-26 23:41:29 +0530

    feat: add type filter buttons

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -1,17 +1,25 @@
 import { useState, useEffect } from "react"
+import { useSearchParams, Link } from "react-router-dom"
+import { FaCircleXmark } from "react-icons/fa6"
 
 import VanTile from "../../components/VanTile"
 
 export default function Vans() {
   const [vans, setVans] = useState([])
-
   useEffect(() => {
     fetch("/api/vans")
       .then((res) => res.json())
       .then((data) => setVans(data.vans))
   }, [])
 
-  const vanElements = vans.map((van) => {
+  const [searchParams, setSearchParams] = useSearchParams()
+  const selectedTypeFilter = searchParams.get("type")
+
+  const filteredVans = selectedTypeFilter
+    ? vans.filter((van) => van.type === selectedTypeFilter)
+    : vans
+
+  const vanElements = filteredVans.map((van) => {
     return (
       <VanTile
         key={van.id}
@@ -28,6 +36,32 @@ export default function Vans() {
     <>
       <main className="main main--vans">
         <h1 className="vans__heading">Explore Our Van Options</h1>
+        <div className="filter-buttons">
+          <Link
+            to="?type=simple"
+            className="filter-button filter-button-simple"
+          >
+            Simple
+          </Link>
+          <Link
+            to="?type=rugged"
+            className="filter-button filter-button-rugged"
+          >
+            Rugged
+          </Link>
+          <Link
+            to="?type=luxury"
+            className="filter-button filter-button-luxury"
+          >
+            Luxury
+          </Link>
+          {selectedTypeFilter && (
+            <Link to="." className="filter-button filter-button-clear">
+              <FaCircleXmark />
+              Clear filters
+            </Link>
+          )}
+        </div>
         <div className="vans__wrapper">{vanElements}</div>
       </main>
     </>
